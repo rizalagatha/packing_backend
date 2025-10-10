3f5f52e6f5865fd37fe243e005feaeaf91a862a1
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2025-10-10 14:10:05 +0700

    Filter SPK

--- src/controllers/spk.controller.js
+++ src/controllers/spk.controller.js
@@ -12,7 +12,7 @@ const findSpkByBarcode = async (req, res) => {
           LEFT JOIN tspk_dc c ON b.brgd_kode = c.spkd_kode
           LEFT JOIN tspk d ON c.spkd_nomor = d.spk_nomor
         WHERE b.brgd_barcode = ?
-        AND spk_close = 0 AND spk_aktif = 'Y';
+        ORDER BY spk_tanggal DESC;
     `;
     const [rows] = await pool.query(query, [barcode]);
     res.status(200).json({ success: true, data: { items: rows } });
