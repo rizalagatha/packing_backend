2fb7d92d122ef99dfd7be0a0a4b5d3bc7bc3d909
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2025-10-18 09:16:11 +0700

    Mutasi Store Kirim

--- src/routes/mutasiStore.routes.js
+++ src/routes/mutasiStore.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const mutasiController = require('../controllers/mutasiStore.controller');
-const { authenticateToken } = require('../middleware/auth.middleware');
+const mutasiController = require("../controllers/mutasiStore.controller")
+const { authenticateToken } = require('../middlewares/auth.middleware');
 
 router.use(authenticateToken);
 
