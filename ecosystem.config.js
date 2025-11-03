bd110ae33fa5fd8aff0390e3fae5d27956218425
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2025-11-03 12:38:36 +0700

    Packing Trial

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,28 +5,30 @@ module.exports = {
       name: "packing-prod",
       script: "index.js",
       env: {
-        "NODE_ENV": "production",
-        "PORT": 3000,
-        "DB_HOST": "localhost",
-        "DB_USER": "root",
-        "DB_PASSWORD": "Kencana#123",
-        "DB_DATABASE": "retail",
-        "DB_PORT": 3307
-      }
+        NODE_ENV: "production",
+        PORT: 3000,
+        DB_HOST: "localhost",
+        DB_USER: "root",
+        DB_PASSWORD: "Kencana#123",
+        DB_DATABASE: "retail",
+        DB_PORT: 3307,
+        JWT_SECRET: "9f5c92853c97dfeec4c7a9d1353dcc30",
+      },
     },
     {
       // --- APLIKASI TRIAL ---
       name: "packing-trial",
       script: "index.js",
       env: {
-        "NODE_ENV": "production", // Tetap production agar efisien
-        "PORT": 3002, // PORT BERBEDA
-        "DB_HOST": "localhost",
-        "DB_USER": "root",
-        "DB_PASSWORD": "Kencana#123",
-        "DB_DATABASE": "retailnew", // DATABASE BERBEDA
-        "DB_PORT": 3307
-      }
-    }
-  ]
+        NODE_ENV: "production", // Tetap production agar efisien
+        PORT: 3002, // PORT BERBEDA
+        DB_HOST: "localhost",
+        DB_USER: "root",
+        DB_PASSWORD: "Kencana#123",
+        DB_DATABASE: "retailnew", // DATABASE BERBEDA
+        DB_PORT: 3307,
+        JWT_SECRET: "9f5c92853c97dfeec4c7a9d1353dcc30",
+      },
+    },
+  ],
 };
