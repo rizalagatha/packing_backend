767ad78095cc867a5a13303d53e4b420c95dc35f
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2025-11-03 12:25:47 +0700

    Packing Trial

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,8 @@ module.exports = {
         "DB_HOST": "localhost",
         "DB_USER": "root",
         "DB_PASSWORD": "Kencana#123",
-        "DB_DATABASE": "retail"
+        "DB_DATABASE": "retail",
+        "DB_PORT": 3307
       }
     },
     {
@@ -23,7 +24,8 @@ module.exports = {
         "DB_HOST": "localhost",
         "DB_USER": "root",
         "DB_PASSWORD": "Kencana#123",
-        "DB_DATABASE": "retailnew" // DATABASE BERBEDA
+        "DB_DATABASE": "retailnew", // DATABASE BERBEDA
+        "DB_PORT": 3307
       }
     }
   ]
