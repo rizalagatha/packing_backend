bd260b00b5fb4008ff14d7aee03934110aa3b2a5
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2025-11-03 12:29:05 +0700

    Packing Trial

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // 1. Impor library yang dibutuhkan
-require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require("express");
 const cors = require("cors");
 const whatsappService = require('./src/services/whatsapp.service');
