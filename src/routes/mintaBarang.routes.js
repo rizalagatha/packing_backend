6ffdc3b6d86c3f5da84665fc751eaf92afcf8a44
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2025-12-01 09:44:26 +0700

    Stok Menipis versi Store

--- src/routes/mintaBarang.routes.js
+++ src/routes/mintaBarang.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mintaController = require('../controllers/mintaBarang.controller');
-const { authenticateToken } = require('../middleware/auth.middleware');
+const { authenticateToken } = require('../middlewares/auth.middleware');
 
 router.use(authenticateToken);
 
