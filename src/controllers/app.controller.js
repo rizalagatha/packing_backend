30ea92526b5a61a7ac7b07d5b1e76b7e23a56656
Rizal Agatha Erdin Agesyah <rizalagatha1403@gmail.com>
2026-05-22 09:30:45 +0700

    1.6.9

--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -4,7 +4,7 @@ const getAppVersion = async (req, res) => {
   try {
     const appInfo = {
       latestVersion: packageJson.version, // Ambil otomatis dari package.json
-      versionCode: 24, // Update angka ini setiap rilis baru di backend
+      versionCode: 26, // Update angka ini setiap rilis baru di backend
       apkUrl: "http://103.94.238.252:3000/public/updates/app-release.apk",
       forceUpdate: false,
       releaseNotes: "Update fitur Bazaar K01 & Perbaikan Bug Scanner",
