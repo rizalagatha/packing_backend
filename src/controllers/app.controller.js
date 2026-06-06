c26a7753e6abe2541d8f74824bc359986742d1c2
Rizal Agatha Erdin Agesyah <rizalagatha1403@gmail.com>
2026-06-06 11:17:05 +0700

    Versi 1.6.16

--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -4,10 +4,10 @@ const getAppVersion = async (req, res) => {
   try {
     const appInfo = {
       latestVersion: packageJson.version, // Ambil otomatis dari package.json
-      versionCode: 31, // Update angka ini setiap rilis baru di backend
+      versionCode: 32, // Update angka ini setiap rilis baru di backend
       apkUrl: "http://103.94.238.252:3000/public/updates/app-release.apk",
       forceUpdate: false,
-      releaseNotes: "Update untuk Stok Opname KDC",
+      releaseNotes: "Update dan Fix untuk Stok Opname KDC",
     };
     res.status(200).json({ success: true, data: appInfo });
   } catch (error) {
