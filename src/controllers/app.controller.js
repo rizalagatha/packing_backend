ec107f2572822e22073516a75ed99fff02346d5e
Rizal Agatha Erdin Agesyah <rizalagatha1403@gmail.com>
2026-06-10 10:29:02 +0700

    1.6.27

--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -4,13 +4,13 @@ const getAppVersion = async (req, res) => {
   try {
     const appInfo = {
       latestVersion: packageJson.version, // Ambil otomatis dari package.json
-      versionCode: 42, // Update angka ini setiap rilis baru di backend
+      versionCode: 43, // Update angka ini setiap rilis baru di backend
       apkUrl: "http://103.94.238.252:3000/public/updates/app-release.apk",
       forceUpdate: false,
       // Ubah dari string tunggal menjadi Array
       releaseNotes: [
-        "Update Urutan Nomor Permintaan di Packing List",
-        "Perbaikan bug dan error",
+        "Menu baru Lost Order dan Riwayat Lost Order, digunakan untuk mencatat lost order dari customer",
+        "Update Terima SJ untuk dapat menerima Accesories dan Obat di update Retail mendatang yang akan distok",
       ],
     };
     res.status(200).json({ success: true, data: appInfo });
