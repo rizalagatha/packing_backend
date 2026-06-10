01bd2e20d0c397f28111a621c22f8c5cfad890ff
Rizal Agatha Erdin Agesyah <rizalagatha1403@gmail.com>
2026-06-10 08:48:25 +0700

    1.6.26

--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -1,13 +1,17 @@
-const packageJson = require("../../package.json"); // Sesuaikan path-nya ke root folder
+const packageJson = require("../../package.json");
 
 const getAppVersion = async (req, res) => {
   try {
     const appInfo = {
       latestVersion: packageJson.version, // Ambil otomatis dari package.json
-      versionCode: 41, // Update angka ini setiap rilis baru di backend
+      versionCode: 42, // Update angka ini setiap rilis baru di backend
       apkUrl: "http://103.94.238.252:3000/public/updates/app-release.apk",
       forceUpdate: false,
-      releaseNotes: "Update Urutan Permintaan Otomatis di Packing List",
+      // Ubah dari string tunggal menjadi Array
+      releaseNotes: [
+        "Update Urutan Nomor Permintaan di Packing List",
+        "Perbaikan bug dan error",
+      ],
     };
     res.status(200).json({ success: true, data: appInfo });
   } catch (error) {
