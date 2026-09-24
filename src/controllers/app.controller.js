7ad4540b988f3e5d12413f0da76302aa02df68cf
Rizal Agatha Erdin Agesyah <rizalagatha1403@gmail.com>
2026-09-24 20:18:58 +0700

    1.6.52

--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -4,14 +4,11 @@ const getAppVersion = async (req, res) => {
   try {
     const appInfo = {
       latestVersion: packageJson.version, // Ambil otomatis dari package.json
-      versionCode: 68, // Update angka ini setiap rilis baru di backend
+      versionCode: 69, // Update angka ini setiap rilis baru di backend
       apkUrl: "http://103.94.238.252:3000/public/updates/app-release.apk",
       forceUpdate: false,
       // Ubah dari string tunggal menjadi Array
-      releaseNotes: [
-        "Setting untuk Kiddify dan Tablet",
-        "Perbaikan untuk Packing List DC",
-      ],
+      releaseNotes: ["Buku Tamu Widget untuk Pameran"],
     };
     res.status(200).json({ success: true, data: appInfo });
   } catch (error) {
